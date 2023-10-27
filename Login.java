1920188843f32dfb598b38ef2437d6466655e339
Rudra Prasadh Ganesh <rudrapra001@e.ntu.edu.sg>
2023-10-27 14:34:19 +0800

    Login class now accepts all logins and checks for validity

---
+++ Login.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Login {
+    static Scanner sc = new Scanner(System.in);
+    static String username = "";
+    public static void main(String[] args) {
+        
+        while (!search(username)){
+            username = "*";
+            while (contains_special(username)){
+                input_username();
+            }
+        }
+        System.out.println("Success! " + username);
+        
+    }
+
+    public static boolean contains_special(String s){
+        for (int i = 0; i < s.length(); i++){
+
+            if (!(Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i)))){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static void input_username(){        
+
+        while ((contains_special(username))){
+            if (username != "*") System.out.println("Please input a valid username!");
+            System.out.println("Input Your Username: ");
+            username = sc.next();
+        }
+    }
+
+    public static boolean search(String s) {
+      try (BufferedReader reader1 = new BufferedReader(new FileReader("staff.txt"));
+           BufferedReader reader2 = new BufferedReader(new FileReader("student.txt"))) {
+  
+          String line;
+          reader1.readLine(); // Skip the first line
+          while ((line = reader1.readLine()) != null) {
+              if (s.equalsIgnoreCase(getusername(line).trim())) {
+                  return true;
+              }
+          }
+  
+          reader2.readLine(); // Skip the first line
+          while ((line = reader2.readLine()) != null) {
+              if (s.equalsIgnoreCase(getusername(line).trim())) {
+                  return true;
+              }
+          }
+  
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  
+      return false;
+  }
+
+    public static String getusername(String s) {
+      String[] parts = s.split("\t");
+      String email = parts[1];
+      int index = email.indexOf('@');
+      String username = email.substring(0,index);
+      return username; 
+  }
+
+}
