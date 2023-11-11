5a956f1d608ee9b45b61c7b246122a3054f8bd45
Esvaran Arun <59063590+esvar2004@users.noreply.github.com>
2023-11-11 19:11:35 +0800

    Add files via upload

--- Enquiry.java
+++ Enquiry.java
@@ -2,22 +2,25 @@
 
 public class Enquiry {
     private String question;
-    private String response; // This field holds the reply from the committee member.
+    private String response; // This field holds the reply from the committee member/staff member.
     private Student postedBy; // Assuming 'Student' is a class representing students.
+    private String campName;
 
-    public Enquiry(String question, Student postedBy) {
+    public Enquiry(String question, Student postedBy, String campName) {
         this.question = question;
         this.postedBy = postedBy;
+        this.campName = campName;
         this.response = ""; // Initially, there's no response until a committee member replies.
     }
 
     // Getter and setter methods
-    public String getQuestion() {
-        return question;
+    public String getCampName()
+    {
+        return campName;
     }
 
-    public void setResponse(String response) {
-        this.response = response;
+    public String getQuestion() {
+        return question;
     }
 
     public String getResponse() {
@@ -26,5 +29,9 @@ public class Enquiry {
 
     public Student getPostedBy() {
         return postedBy;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
     }
 }
