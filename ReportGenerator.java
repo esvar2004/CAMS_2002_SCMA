f9ce9204cdfa81a530088bfe1efcade9dabb3f73
Esvaran Arun <59063590+esvar2004@users.noreply.github.com>
2023-11-26 00:57:35 +0800

    Add files via upload

--- ReportGenerator.java
+++ ReportGenerator.java
@@ -1,3 +1,4 @@
+//package a2002Assignment;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -6,9 +7,20 @@ public class ReportGenerator {
     public void generateCampReport(Camp camp) {
         try (FileWriter writer = new FileWriter(camp.getName() + "_report.txt")) {
             writer.write("Camp Report for: " + camp.getName() + "\n");
+            writer.write("Open Date: " + camp.getOpenDate() + "\n");
+            writer.write("Close Date: " + camp.getCloseDate() + "\n");
+            writer.write("Faculty: " + camp.getSchool() + "\n");
+            writer.write("Location " + camp.getLocation() + "\n");
             writer.write("Total Slots: " + camp.getTotSlots() + "\n");
+            writer.write("Remaining Slots: " + camp.getAvailableSlots() + "\n");
             writer.write("Committee Slots: " + camp.getCommSlots() + "\n");
             writer.write("Description: " + camp.getDesc() + "\n");
+
+            for(int i = 0; i < camp.getList().size(); i++)
+            {
+                writer.write("Student " + i + ": " + camp.getList().get(i).getName() + ", ");
+                writer.write("Role: " + camp.getList().get(i).getRole() + "\n");
+            }
             // More details can be included
         } catch (IOException e) {
             e.printStackTrace();
